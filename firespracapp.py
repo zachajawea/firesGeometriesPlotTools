ba2c9cda0034060b60acf6d35c2fed2d4311fa97
zachajawea <zachcammer@gmail.com>
2022-03-16 11:50:41 -0400

    initial commit
    added python file

---
+++ firespracapp.py
@@ -0,0 +1,25 @@
+#TODO Weapon Database
+
+import math
+
+class worm():
+# W = R/1000 * M
+
+    def findW(self,R,M):
+        return (R/1000) * M
+
+    def findR(self,W,M):
+        return (W/M) * 1000
+
+    def findM(self,W,R):
+        return (W*1000) / R
+
+class batwing():
+
+    #TODO find p
+    #TODO find q
+    #TODO find distance y
+    #TODO find distance x
+    #TODO 
+
+if __name__ == "__main__":
